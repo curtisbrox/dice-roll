fb02502bf5be5cf1ca820818a417b1cfc33c4905
Curt <curt@Curts-MacBook-Pro.local>
2020-01-23 13:04:24 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*
+Problem A: Randomize a dice rool between 1 and 6
+*/
+
+let roll = 2
+
+
+
+/*
+Problem B: Put the roll number into the output
+1. Find the <output> element
+2. Change the content of the <output> element
+*/
+
+let theOutput = document.querySelector('.output')
+
+theOutput.innerHTML = `You rolled: ${roll}`
+
+/*
+Problem C: Change the image to reflect the roll
+1. Find the <img> element
+2. Change the `src` attribute of the <img> element
+*/
+
+
+
+/*
+Problem D: Make the button clickable to run A, B, C together
+1. Find the <button> element
+2. Add a listener to the <button>
+3. Paste the soliution for A, B, C (above) into the event handler function
+*/
